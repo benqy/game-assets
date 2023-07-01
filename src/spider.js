db68a01eed58a1cef233ca5c6c6b441f6a2e0b8c
benqy <hmjlr123@gmail.com>
2023-07-01 15:05:06 +0800

    1

--- src/spider.js
+++ src/spider.js
@@ -1,51 +1,58 @@
 import { urls } from './data.js'
 import axios from 'axios'
+import fs from 'fs'
 
 const getFileNameFromUrl = (url) => {
   const urlParts = url.split('/')
-  return urlParts[urlParts.length - 1]
+  return urlParts[urlParts.length - 1].split('?')[0].replace(/.*-zagorulko-/,'').replace(/.*-zahorulko-/,'')
 }
 
-const downloadImage = (url) => {
+const fails = []
+
+const downloadImage = async (url) => {
   const imagePath = getFileNameFromUrl(url)
-  axios({
-    url,
-    responseType: 'stream',
-  }).then(
-    (response) =>
-      new Promise((resolve, reject) => {
-        response.data
-          .pipe(fs.createWriteStream(imagePath))
-          .on('finish', () => resolve())
-          .on('error', (e) => reject(e))
-      })
-  )
+  try{
+  const res = await axios.get(url, {
+    proxy: {
+      protocol: 'http',
+      host: '127.0.0.1',
+      port: 7890,
+    },
+    responseType: 'arraybuffer', // 特别注意，需要加上此参数
+  });
+
+//   console.log(res.data);
+  fs.writeFileSync(`./download/${imagePath}`, res.data);
+}catch(ex){
+  console.log(ex.toString())
+  fails.push(url)
+
+}
+  // instance.get({
+  //   url,
+  //   config:{
+  //     responseType: 'arraybuffer',
+  //   }
+  // }).then((response) => {
+  //   // console.log(response.data)
+  //   new Promise((resolve, reject) => {
+  //     response.data
+  //       .pipe(fs.createWriteStream(imagePath))
+  //       .on('finish', () => resolve())
+  //       .on('error', (e) => reject(e))
+  //   })
+  // })
 }
 
 const spider = async () => {
-  const imageUrls = urls.splice(0, 2)
-  let example_image_1 = await downloadImage(
-    imageUrls[0]
-  )
-
-  console.log(example_image_1.status) // true
-  console.log(example_image_1.error) // ''
-
-  // let example_image_2 = await download_image(
-  //   'https://example.com/does-not-exist.png',
-  //   'example-2.png'
-  // )
-
-  // console.log(example_image_2.status) // false
-  // console.log(example_image_2.error) // 'Error: Request failed with status code 404'
-
-  // let example_image_3 = await download_image(
-  //   'https://example.com/test-3.png',
-  //   'example-3.png'
-  // )
-
-  // console.log(example_image_3.status) // true
-  // console.log(example_image_3.error) // ''
+  // const imageUrls = urls.splice(i, n)
+  for(let i = 0; i < urls.length; i++){
+    await downloadImage(urls[i])
+    console.log(`${i}: ${urls[i]}`)
+  }
+  console.log(fails)
 }
 
 spider()
+// spider(400)
+// spider(500)
